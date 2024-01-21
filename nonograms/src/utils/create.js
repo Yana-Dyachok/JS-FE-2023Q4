4c3dbaf1ca12e53a5b0ac544280c927919b7b017
Yana-Dyachok <annetart1994@gmail.com>
2024-01-21 23:08:18 +0200

    feat: add styles to popup and header

--- nonograms/src/utils/create.js
+++ nonograms/src/utils/create.js
@@ -1,35 +1,36 @@
 // create header-----------------------------------------------------------------------------------------------------------------------------------------------------------------
-document.body.setAttribute("class", "body change");
-const wrapperBlock = document.createElement("div");
-wrapperBlock.setAttribute("class", "wrapper");
+document.body.setAttribute('class', 'body change');
+const wrapperBlock = document.createElement('div');
+wrapperBlock.setAttribute('class', 'wrapper');
 document.body.append(wrapperBlock);
-const header = document.createElement("header");
-header.setAttribute("class", "header");
-const scoreboard= document.createElement("div");
-scoreboard.setAttribute("class", "header__scoreboard scoreboard");
+const header = document.createElement('header');
+header.setAttribute('class', 'header');
+const scoreboard = document.createElement('div');
+scoreboard.setAttribute('class', 'header__scoreboard scoreboard');
 //first row  - level of game-----------------------------------------------------------------------------------------------------------------------------------------
-const firstRow=document.createElement("div");
-firstRow.setAttribute("class", "scoreboard__first-row");
+const firstRow = document.createElement('div');
+firstRow.setAttribute('class', 'scoreboard__rows scoreboard__first-row');
 //level----------------------------------------------------------------------------------------------------------------------------------------------------
-const levelBlock= document.createElement("div");
-levelBlock.setAttribute("class", "level-block");
-const levelText= document.createElement("div");
-levelText.setAttribute("class", "menu-title");
-levelText.textContent ="Level: ";
+const levelBlock = document.createElement('div');
+levelBlock.setAttribute('class', 'scoreboard__level blocks');
+const levelText = document.createElement('div');
+levelText.setAttribute('class', 'scoreboard__level__title');
+levelText.textContent = 'Level: ';
 levelBlock.append(levelText);
-export const level= document.createElement("select");
-level.setAttribute("class", "level");
-const option1 = document.createElement("option");
-option1.value = "value1";
-option1.textContent = "easy";
+export const level = document.createElement('select');
+level.setAttribute('class', 'scoreboard__level__select');
 
-const option2 = document.createElement("option");
-option2.value = "value2";
-option2.textContent = "medium";
+const option1 = document.createElement('option');
+option1.value = 'value1';
+option1.textContent = 'easy';
 
-const option3 = document.createElement("option");
-option3.value = "value3";
-option3.textContent = "hard";
+const option2 = document.createElement('option');
+option2.value = 'value2';
+option2.textContent = 'medium';
+
+const option3 = document.createElement('option');
+option3.value = 'value3';
+option3.textContent = 'hard';
 
 level.appendChild(option1);
 level.appendChild(option2);
@@ -37,50 +38,100 @@ level.appendChild(option3);
 levelBlock.append(level);
 firstRow.append(levelBlock);
 
+const randomBtn = document.createElement('button');
+randomBtn.setAttribute('class', 'scoreboard__random-btn btn');
+randomBtn.textContent='Random game'
+firstRow.append(randomBtn);
+
+//picture----------------------------------------------------------------------------------------------------------------------------------------------------
+const pictureBlock = document.createElement('div');
+pictureBlock.setAttribute('class', 'scoreboard__picture blocks');
+
+const pictureText = document.createElement('div');
+pictureText.setAttribute('class', 'scoreboard__picture__title');
+pictureText.textContent = 'Picture: ';
+pictureBlock.append(pictureText);
+
+export const picture = document.createElement('select');
+picture.setAttribute('class', 'scoreboard__picture__select');
+
+const pictureOptions = [
+  { value: 'pictureValue1', text: 'animals' },
+  { value: 'pictureValue2', text: 'plants' },
+  { value: 'pictureValue3', text: 'different subjects'},
+  { value: 'pictureValue4', text: 'geometric shapes'},
+  { value: 'pictureValue5', text: 'food' }
+];
+
+for (const optionData of pictureOptions) {
+  const pictureOption = document.createElement('option');
+  pictureOption.value = optionData.value;
+  pictureOption.textContent = optionData.text;
+  picture.appendChild(pictureOption);
+}
+
+pictureBlock.append(picture);
+firstRow.append(pictureBlock);
+
 //second row - theme toggle, mike-------------------------------------------------------------------------------------------------------------
-const secondRow=document.createElement("div");
-secondRow.setAttribute("class", "scoreboard__second-row");
+const secondRow = document.createElement('div');
+secondRow.setAttribute('class', 'scoreboard__rows scoreboard__second-row');
 //theme toggle
-export const themeToggle= document.createElement("div");
-themeToggle.setAttribute("class", "scoreboard__theme-toggle change");
+export const themeToggle = document.createElement('div');
+themeToggle.setAttribute('class', 'scoreboard__theme-toggle change');
 secondRow.append(themeToggle);
 
+// time of game-----------------------------------------------------------------------------------------------------------------
+const timeOfGame = document.createElement('div');
+timeOfGame.setAttribute('class', 'scoreboard__time blocks');
+const timeText = document.createElement('div');
+timeText.setAttribute('class', 'scoreboard__time__title');
+timeOfGame.append(timeText);
+timeText.textContent = 'Time: ';
+export const date = document.createElement('div');
+date.setAttribute('class', 'scoreboard__time__date change');
+date.textContent = '00:00';
+timeOfGame.append(date);
+secondRow.append(timeOfGame);
+
 // audio------------------------------------------------------------------------------------------------------------
-export const mike= document.createElement("div");
-mike.setAttribute("class", "scoreboard__mike change");
-export const audioLose = document.createElement("scoreboard__audio");
-audioLose.setAttribute("class", "scoreboard__audio--lose");
-audioLose.src ="./src/assets/audio/lose.mp3"
+export const mike = document.createElement('div');
+mike.setAttribute('class', 'scoreboard__mike change');
+export const audioLose = document.createElement('scoreboard__audio');
+audioLose.setAttribute('class', 'scoreboard__audio--lose');
+audioLose.src = './src/assets/audio/lose.mp3';
 mike.append(audioLose);
-export const audioWin = document.createElement("scoreboard__audio");
-audioWin.setAttribute("class", "scoreboard__audio--win");
-audioWin.src ="./src/assets/audio/win.mp3"
+export const audioWin = document.createElement('scoreboard__audio');
+audioWin.setAttribute('class', 'scoreboard__audio--win');
+audioWin.src = './src/assets/audio/win.mp3';
 mike.append(audioWin);
-export const audiMark = document.createElement("scoreboard__audio");
-audiMark.setAttribute("class", "scoreboard__audio--mark");
-audiMark.src ="./src/assets/audio/point-cell.mp3"
+export const audiMark = document.createElement('scoreboard__audio');
+audiMark.setAttribute('class', 'scoreboard__audio--mark');
+audiMark.src = './src/assets/audio/point-cell.mp3';
 mike.append(audiMark);
-export const audioFlag= document.createElement("scoreboard__audio");
-audioFlag.setAttribute("class", "scoreboard__audio--flag");
-audioFlag.src ="./src/assets/audio/flag.mp3"
+export const audioFlag = document.createElement('scoreboard__audio');
+audioFlag.setAttribute('class', 'scoreboard__audio--flag');
+audioFlag.src = './src/assets/audio/flag.mp3';
 mike.append(audioFlag);
 secondRow.append(mike);
 
 //third row - time-----------------------------------------------------------------------------------------------------
-const thirdRow=document.createElement("div");
-thirdRow.setAttribute("class", "scoreboard__third-row");
-// time of game-----------------------------------------------------------------------------------------------------------------
-const timeOfGame= document.createElement("div");
-timeOfGame.setAttribute("class", "scoreboard__time");
-const timeText= document.createElement("div");
-timeText.setAttribute("class", "scoreboard__time__title");
-timeOfGame.append(timeText);
-timeText.textContent ="Time: ";
-export const date= document.createElement("div");
-date.setAttribute("class", "scoreboard__time__date change");
-date.textContent = "00:00";
-timeOfGame.append(date);
-thirdRow.append(timeOfGame);
+const thirdRow = document.createElement('div');
+thirdRow.setAttribute('class', 'scoreboard__rows scoreboard__third-row');
+const resetGameBtn = document.createElement('button');
+resetGameBtn.setAttribute('class', 'scoreboard__reset-btn btn');
+resetGameBtn.textContent='Reset'
+thirdRow.append(resetGameBtn);
+
+const continueBtn = document.createElement('button');
+continueBtn.setAttribute('class', 'scoreboard__continue-btn btn');
+continueBtn.textContent='Continue'
+thirdRow.append(continueBtn);
+
+const solutionBtn = document.createElement('button');
+solutionBtn.setAttribute('class', 'scoreboard__solution-btn btn');
+solutionBtn.textContent='Solution'
+thirdRow.append(solutionBtn);
 
 scoreboard.append(firstRow);
 scoreboard.append(secondRow);
@@ -89,25 +140,25 @@ header.append(scoreboard);
 wrapperBlock.append(header);
 
 //create main----------------------------------------------------------------------------------------------------------------------------
-const main= document.createElement("main");
-main.setAttribute("class", "main");
-export const gameBlock= document.createElement("div");
-gameBlock.setAttribute("class", "game__block");
-const gameInner= document.createElement("div");
-gameInner.setAttribute("class", "game__inner");
-export const gameTable= document.createElement("table");
-gameTable.setAttribute("id", "game");
+const main = document.createElement('main');
+main.setAttribute('class', 'main');
+export const gameBlock = document.createElement('div');
+gameBlock.setAttribute('class', 'game__block');
+const gameInner = document.createElement('div');
+gameInner.setAttribute('class', 'game__inner');
+export const gameTable = document.createElement('table');
+gameTable.setAttribute('id', 'game');
 gameInner.append(gameTable);
 main.append(gameBlock);
 main.append(gameInner);
 wrapperBlock.append(main);
 
 //create footer-----------------------------------------------------------------------------------------------------------------------------------------------------
-const footer= document.createElement("footer");
-footer.setAttribute("class", "footer");
+const footer = document.createElement('footer');
+footer.setAttribute('class', 'footer');
 wrapperBlock.append(footer);
 export default wrapperBlock;
 
-export const overlay= document.createElement("div");
- overlay.setAttribute("class", "overlay");
+export const overlay = document.createElement('div');
+overlay.setAttribute('class', 'overlay');
 document.body.append(overlay);
--- nonograms/src/utils/popup.js
+++ nonograms/src/utils/popup.js
@@ -1,35 +1,33 @@
-export function createPopUp(flag) {
-    const popup = document.createElement("div");
-    popup.classList.add("popup");
-  
-    const popupBody = document.createElement("div");
-    popupBody.classList.add("popup__body");
-  
-    const popupContent = document.createElement("div");
-    popupContent.classList.add("popup__content");
-  
-    const popupText = document.createElement("h3");
-    popupText.classList.add("popup__text");
-    popupText.textContent = flag
-      ? "Сongratulations! You won!"
-      : "Unfortunately, you lost!";
-  
-    const closeButton = document.createElement("button");
-    closeButton.classList.add("popup__close-btn");
-    closeButton.textContent = "New Game";
-  
-    closeButton.addEventListener("click", () => {
-      popup.classList.toggle("target");
-      newGame();
+export function createPopUp(flag, time) {
+    const popup = document.createElement('div');
+    popup.setAttribute('class', 'popup');
+
+    const popupBody = document.createElement('div');
+    popupBody.setAttribute('class', 'popup__body');
+
+    const popupContent = document.createElement('div');
+    popupContent.setAttribute('class', 'popup__content');
+
+    const popupText = document.createElement('h3');
+    popupText.setAttribute('class', 'popup__text');
+    popupText.innerHTML = flag
+        ? `Great! You have solved the nonogram in <span class="popup__span-word">'${time}'</span> seconds!`
+        : 'Unfortunately, you lost!';
+
+    const closeButton = document.createElement('button');
+    closeButton.setAttribute('class', 'popup__close-btn btn');
+    closeButton.textContent = 'New Game';
+
+    closeButton.addEventListener('click', () => {
+        popup.classList.toggle('target');
+        newGame();
     });
-  
-    showWord.append(spanWord);
+
     popupContent.append(popupText);
-    popupContent.append(showWord);
     popupContent.append(closeButton);
-  
+
     popupBody.append(popupContent);
-  
+
     popup.append(popupBody);
     document.body.append(popup);
-  }
+}
