bb3be2011c51338016219ab77c2a78a1778efdc5
Yana-Dyachok <annetart1994@gmail.com>
2024-01-24 22:41:51 +0200

    feat: add convert to s funct and flags

--- nonograms/src/utils/popup.js
+++ nonograms/src/utils/popup.js
@@ -1,4 +1,8 @@
+import { startGame, flags  } from './game.js';
+
 export function createPopUp(flag, time) {
+    time = convertToSeconds(time);
+
     const popup = document.createElement('div');
     popup.setAttribute('class', 'popup');
 
@@ -6,7 +10,7 @@ export function createPopUp(flag, time) {
     popupBody.setAttribute('class', 'popup__body');
 
     const popupContent = document.createElement('div');
-    popupContent.setAttribute('class', 'popup__content change');
+    popupContent.setAttribute('class', `popup__content ${flags.themeFlag === 'true' ? 'dark' : ''}`);
 
     const popupText = document.createElement('h3');
     popupText.setAttribute('class', 'popup__text');
@@ -15,12 +19,11 @@ export function createPopUp(flag, time) {
         : 'Unfortunately, you lost!';
 
     const closeButton = document.createElement('button');
-    closeButton.setAttribute('class', 'popup__close-btn btn change');
+    closeButton.setAttribute('class', `popup__close-btn btn ${flags.themeFlag === 'true' ? 'dark' : ''}`);
     closeButton.textContent = 'New Game';
-
     closeButton.addEventListener('click', () => {
         popup.classList.toggle('target');
-        //  newGame();
+        startGame();
     });
 
     popupContent.append(popupText);
@@ -30,4 +33,10 @@ export function createPopUp(flag, time) {
 
     popup.append(popupBody);
     document.body.append(popup);
+}
+
+function convertToSeconds(timeString) {
+    const [minutes, secundes] = timeString.split(':').map(Number);
+    const totalSeconds = minutes * 60 + secundes;
+    return totalSeconds;
 }
