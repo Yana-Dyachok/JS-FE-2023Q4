2ea14ea60f700ccc19d35215f2f7c4c5398d61f4
Yana-Dyachok <annetart1994@gmail.com>
2024-02-26 16:47:55 +0200

    fix: import interfaces and change code in drawNews, drawSources

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,15 +1,6 @@
 import News from './news/news';
-import { NewsItem } from './news/news';
 import Sources from './sources/sources';
-import { Source } from './sources/sources';
-
-export interface NewsData {
-    articles: NewsItem[];
-}
-
-export interface SourcesData {
-    sources: Source[];
-}
+import { SourcesData, NewsData } from '../types/interfaces';
 
 export class AppView {
     private news: News;
@@ -21,12 +12,12 @@ export class AppView {
     }
 
     drawNews(data: NewsData): void {
-        const values = data.articles || [];
+        const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
     drawSources(data: SourcesData): void {
-        const values = data.sources || [];
+        const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
 }
