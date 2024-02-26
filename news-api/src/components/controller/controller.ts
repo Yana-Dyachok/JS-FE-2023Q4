3b822eba287e6e0a6a010a38fd193608ab486c35
Yana-Dyachok <annetart1994@gmail.com>
2024-02-26 16:53:03 +0200

    fix: import intrface and change datas types

--- news-api/src/components/controller/controller.ts
+++ news-api/src/components/controller/controller.ts
@@ -1,14 +1,12 @@
 import AppLoader from './appLoader';
-
-import { NewsItem } from '../view/news/news';
-import { Source } from '../view/sources/sources';
+import { SourcesData, NewsData } from '../types/interfaces';
 
 class AppController extends AppLoader {
-    getSources(callback: (data: Source[]) => void): void {
+    getSources(callback: (data: SourcesData) => void): void {
         super.getResp({ endpoint: 'sources' }, callback);
     }
 
-    getNews(e: Event, callback: (data: NewsItem[]) => void): void {
+    getNews(e: Event, callback: (data: NewsData) => void): void {
         let target = e.target as HTMLElement;
         const newsContainer = e.currentTarget as HTMLElement;
 
@@ -39,6 +37,7 @@ class AppController extends AppLoader {
 }
 
 export default AppController;
+
 // js code
 // class AppController extends AppLoader {
 //     getSources(callback) {
