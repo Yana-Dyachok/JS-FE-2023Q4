dee7f05263c1b62fefb853e99857c99c8b14a91d
Yana-Dyachok <annetart1994@gmail.com>
2024-02-28 18:43:35 +0200

    feat: add lint-fix in scripts

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -1,6 +1,6 @@
 import AppController from '../controller/controller';
 
-import { AppView} from '../view/appView';
+import { AppView } from '../view/appView';
 
 class App {
     private controller: AppController;
