27d8b212bca925ce2ee0a07f9a70098f94d78c32
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2024-03-18 01:07:39 +0200

    feat: RSS-PZ-10 enable reversible interaction with word cards (#34)

--- rss-puzzle/src/components/play-field/result-block/result_block.ts
+++ rss-puzzle/src/components/play-field/result-block/result_block.ts
@@ -1,5 +1,4 @@
 import { ILevelData } from '../../../interfaces/interfaces';
-
 import './result_block.scss';
 
 class ResultBlock {
@@ -10,11 +9,25 @@ class ResultBlock {
         this.resultBlock.classList.add('play-field_result');
     }
 
+    private onClick(card: HTMLDivElement): void {
+        const sourceCards: HTMLDivElement | null = document.querySelector('.play-field_source');
+        const newSourceCard = document.createElement('div');
+        newSourceCard.classList.add('play-field_source-card');
+        newSourceCard.setAttribute('data-source', card.dataset.result || '');
+        newSourceCard.textContent = card.textContent || '';
+        sourceCards!.append(newSourceCard);
+        card.parentElement!.removeChild(card);
+    }
+
     createResultBlock(): HTMLDivElement {
         for (let i = 0; i < 10; i++) {
             const level = document.createElement('div');
             level.classList.add('play-field_result-level');
             level.setAttribute('data-result-level', `${i}`);
+            level.onclick = (event) => {
+                const clickedChild = event.target as HTMLDivElement;
+                if (clickedChild instanceof HTMLDivElement) this.onClick(clickedChild);
+            };
             this.resultBlock.append(level);
         }
 
@@ -22,7 +35,7 @@ class ResultBlock {
     }
 
     getRootElement(): HTMLDivElement {
-      return this.createResultBlock();
+        return this.createResultBlock();
     }
 }
 
